d80111d2c6c8446d68cb5eecd5675fa6856e7d74
Brett van Beynum <maildrop444@gmail.com>
2020-04-09 08:24:35 -0400

    Add script to randomly select videos

---
+++ scripts/python/videopicker.py
@@ -0,0 +1,30 @@
+import os
+import re
+import random
+import time
+import requests
+
+startTime = time.time()
+
+#"./files/d/2019-10-19", "./files/d/2019-10-27", "./files/e/2019-10-05", "./files/e/2019-09-28", "./files/e/2019-09-21", "./files/e/2019-09-14"
+folders = ["./files/d/2019-10-12"]
+
+for folder in folders:
+	files = []
+
+	for file in os.listdir(folder):
+		if (re.search(r"([^.]+).mp4", file, flags=re.IGNORECASE)):
+			files.append(folder + "/" + file)
+
+	randomFiles = random.sample(files, int(len(files) * .2))
+
+	print(str(round(time.time() - startTime)) + " uploading " + str(len(randomFiles)) + " of " + str(len(files)) + " from " + folder)
+
+	for index, randomFile in enumerate(randomFiles):
+		print(str(round(time.time() - startTime)) + " uploading " + index + ": " + randomFile)
+		fileReader = open(randomFile, "rb")
+		uploadFile = { randomFile: fileReader }
+		response = requests.post("http://huntingtonbeach.beynum.com/uploadvideo", files=uploadFile)
+		fileReader.close()
+
+	print(str(round(time.time() - startTime)) + " completed " + folder)
