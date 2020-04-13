9a03cfb31aaee1ceb67ab435a848f74670d6badd
Brett van Beynum <maildrop444@gmail.com>
2020-04-13 09:43:55 -0400

    added picker to choose from videos

--- scripts/python/videopicker.py
+++ scripts/python/videopicker.py
@@ -6,8 +6,9 @@ import requests
 
 startTime = time.time()
 
-#"./files/d/2019-10-19", "./files/d/2019-10-27", "./files/e/2019-10-05", "./files/e/2019-09-28", "./files/e/2019-09-21", "./files/e/2019-09-14"
-folders = ["./files/d/2019-10-12"]
+folders = ["./files/e/2019-10-05", "./files/e/2019-09-28", "./files/e/2019-09-21", "./files/e/2019-09-14"]
+
+# "./files/d/2019-10-12", "./files/d/2019-10-19", "./files/d/2019-10-27"
 
 for folder in folders:
 	files = []
@@ -21,7 +22,7 @@ for folder in folders:
 	print(str(round(time.time() - startTime)) + " uploading " + str(len(randomFiles)) + " of " + str(len(files)) + " from " + folder)
 
 	for index, randomFile in enumerate(randomFiles):
-		print(str(round(time.time() - startTime)) + " uploading " + index + ": " + randomFile)
+		print(str(round(time.time() - startTime)) + " uploading " + str(index + 1) + ": " + randomFile)
 		fileReader = open(randomFile, "rb")
 		uploadFile = { randomFile: fileReader }
 		response = requests.post("http://huntingtonbeach.beynum.com/uploadvideo", files=uploadFile)
