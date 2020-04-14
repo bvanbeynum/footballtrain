a721b344a1a3e21f9f5da18203374ac0aa16ed44
Brett van Beynum <maildrop444@gmail.com>
2020-04-14 07:33:50 -0400

    Download to new location

--- scripts/python/download.py
+++ scripts/python/download.py
@@ -2,17 +2,25 @@ import requests
 import json
 import shutil
 import os
+import time
 
-response = requests.get("http://dev.beynum.com:8080/api/allimages")
+startTime = time.time()
+
+response = requests.get("http://huntingtonbeach.beynum.com/api/allimages")
 files = json.loads(response.text)
 
-for image in files["images"]:
-    response = requests.get("http://dev.beynum.com:8080/image/" + image, stream = True)
+print(str(round(time.time() - startTime)) + " - Files to load: " + str(len(files["images"])))
 
-    with open(os.curdir + "/frames/complete/" + image, "wb") as filePointer:
+for index, image in enumerate(files["images"]):
+    response = requests.get("http://huntingtonbeach.beynum.com/image/" + image, stream = True)
+
+    with open(os.curdir + "/frames/" + image, "wb") as filePointer:
         response.raw.decode_content = True
         shutil.copyfileobj(response.raw, filePointer)
     
-    requests.delete("http://dev.beynum.com:8080/image/" + image)
+    requests.delete("http://huntingtonbeach.beynum.com/image/" + image)
+
+    if index % 200 == 0:
+        print(str(round(time.time() - startTime)) + " - file " + str(index) + " of " + str(len(files["images"])))
 
 print("Completed")
